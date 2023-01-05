12d56d7d048823079839ab1f608fee3bc5631021
Garrit <garrit.schaap@ju.se>
2023-01-05 16:32:20 +0100

    Added internal working log to iframe

--- src/client/render.ts
+++ src/client/render.ts
@@ -25,26 +25,53 @@ export function render({ container, mime, value }: IRenderInfo) {
   }
   if (sizeArray[1] === "innerHeight" || sizeArray[1] === "outerHeight") {
     sizeArray[1] = "300px";
+  } else {
+    sizeArray[1] = `${parseInt(sizeArray[1]) + 100}px`;
   }
 
   const iframe = document.createElement("iframe");
-  // iframe.width = "100%";
+  iframe.width = "100%";
   // iframe.height = "300px";
-  iframe.width = sizeArray[0];
+  //iframe.width = sizeArray[0];
   iframe.height = sizeArray[1];
+  //iframe.style.height = sizeArray[1];
   iframe.style.border = "none";
   iframe.style.margin = "0px";
   iframe.style.padding = "0px";
   const iroot = document.createElement("div");
+  iroot.style.lineHeight = "0px";
+
+  const heightContainer = document.createElement("div");
+  //heightContainer.style.height = sizeArray[1];
+  heightContainer.style.overflow = "hidden";
+  heightContainer.style.display = "flex";
+  heightContainer.style.flexDirection = "column";
 
   const postcode = `
+  function p5setup() {
+    const createCanvasSuper = window.createCanvas;
+    window.createCanvas = (w, h, renderer) => {
+      document.getElementById("p5container").innerHTML = "";
+      const p5canvas = createCanvasSuper(w, h, renderer);
+      p5canvas.parent("p5container");
+      return p5canvas;
+    };
+
+    window.customSetup();
+  }
+
+  window.customSetup = window.setup;
+  window.setup = p5setup;
+
   window.addEventListener("load", () => {
     document.querySelector("body").style.margin = "0px auto";
-    document.querySelector("body").style.lineHeight = "0px";
+    //document.querySelector("body").style.lineHeight = "0px";
   });
 
   function sendLog(message, type) {
-    window.parent.postMessage({ type: "p5log", message: message, logType: type }, "*");
+    const logElement = document.getElementById("p5log");
+    logElement.innerHTML += message + "<br>";
+    logElement.scrollTop = logElement.scrollHeight;
   }
   
   function addLog(msg, type) {
@@ -77,8 +104,21 @@ export function render({ container, mime, value }: IRenderInfo) {
   const script = document.createElement("script");
   script.innerHTML = `${value}\n\n${p5string}\n\n${postcode}`;
   script.style.margin = "0px";
-  iroot.appendChild(script);
+  heightContainer.appendChild(script);
 
+  const p5container = document.createElement("div");
+  p5container.id = "p5container";
+  //p5container.style.position = "absolute";
+  //p5container.innerText = "CONTAINER";
+  heightContainer.appendChild(p5container);
+
+  const logElement = document.createElement("code");
+  logElement.id = "p5log";
+  logElement.style.height = "100px";
+  logElement.style.overflow = "auto";
+  heightContainer.appendChild(logElement);
+
+  iroot.appendChild(heightContainer);
   iframe.srcdoc = iroot.innerHTML;
   container.appendChild(iframe);
 
